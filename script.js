b94ce7a0f9939a595579db35dad8d602cca421ed
Kanagamalar <kmalar2103@gmail.com>
2026-09-11 10:20:06 +0530

    Build resume frontend

---
+++ script.js
@@ -0,0 +1,12 @@
+// ================================
+// TEMPORARY VISITOR COUNTER
+// ================================
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const visitorCount =
+        document.getElementById("visitor-count");
+
+    visitorCount.textContent = "0";
+
+});
